943b7c76593c41057bcf4692ff6c7f6267740682
Ishwarya26 <24wh1a0581@bvrithyderabad.edu.in>
2026-04-17 09:59:40 +0530

    Create Insertion_Employee.java

---
+++ Insertion_Employee.java
@@ -0,0 +1,66 @@
+package ebook581;
+
+
+
+public class Employee {
+
+    private int empid;
+
+    private String name;
+
+    private int salary;
+
+    private String department;
+
+
+
+    public Employee(int empid, String name, int salary, String department) {
+
+        this.empid = empid;
+
+        this.name = name;
+
+        this.salary = salary;
+
+        this.department = department;
+
+    }
+
+
+
+    public int getEmpid() {
+
+        return empid;
+
+    }
+
+
+
+    public String getName() {
+
+        return name;
+
+    }
+
+
+
+    public int getSalary() {
+
+        return salary;
+
+    }
+
+
+
+    public String getDepartment() {
+
+        return department;
+
+    }
+
+}
+
+
+
+
+
